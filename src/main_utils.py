0375101a5dd059f79efc2ae19f92e73556fd2ecb
sanjota1234 <sanjotanandagav97@gmail.com>
2025-07-22 11:31:45 +0530

    Create main_utils.py

---
+++ src/main_utils.py
@@ -0,0 +1,11 @@
+def add(a,b):
+  """
+  adds two numbers and returns thr results
+  parameters:
+  a(int):first number
+  b(int):second number
+  returns:
+  int:sum of a and b
+  """
+  return a+b
+        
